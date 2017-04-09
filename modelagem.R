59e019cf10a0f790f5d6bb91e5dd2d22b96b8616
diogosbr <diogosbr@gmail.com>
2017-04-09 20:11:19 -0300

    Update modelagem.R

--- modelagem.R
+++ modelagem.R
@@ -379,8 +379,9 @@ modelos=function(coord,k=3,diretorio="teste",plot=T,
       
    
   dir.create("./temp")
+
   for(i in length(names(final))){
-    writeRaster(paste0(temp[[i]],paste0("./temp/",names(final)[i])))
+    writeRaster(paste0(final[[i]],paste0("./temp/",names(final)[i],".tif")),format="GTiff)
   }
   
   final2=stack(list.files("./temp",pattern = ".tif",full.names = T))
