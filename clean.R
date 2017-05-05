396c557eff343313cc7b42d5d1cd95f204c09736
diogosbr <diogosbr@gmail.com>
2017-05-05 11:35:27 -0300

    Adicionando um 'stop' no final

--- clean.R
+++ clean.R
@@ -1,19 +1,19 @@
 clean=function(coord,predictors){
   if(dim(coord)[2]==2){
     if(exists('predictors')){
-      #selecionar os pontos únicos e sem NA
+      #selecionar os pontos Ãºnicos e sem NA
       pts=coord
       mask=predictors[[1]]
-      # Selecionar pontos espacialmente únicos #
+      # Selecionar pontos espacialmente Ãºnicos #
       cell <- cellFromXY(mask, pts) # get the cell number for each point
       dup <- duplicated(cell)
       pts1 <- pts[!dup,]# select the records that are not duplicated
       pts1 <- pts1[!is.na(extract(mask,pts1)),] #selecionando apenas pontos que tem valor de raster
       
       cat(dim(pts)[1]-dim(pts1)[1],"pontos retirados\n")
-      cat(dim(pts1)[1],"pontos espacialmente únicos\n")
+      cat(dim(pts1)[1],"pontos espacialmente Ãºnicos\n")
       #pts1
       return(pts1)
-    }else(cat("Indique o objeto com as variáveis preditoras"))
-  }else(cat("Tabela de coordenadas tem mais de duas colunas.\nEsta tabela deve ter apenas long e lat, nesta ordem."))
+    }else(cat("Indique o objeto com as variÃ¡veis preditoras"))
+  }else(stop("Tabela de coordenadas tem mais de duas colunas.\nEsta tabela deve ter apenas long e lat, nesta ordem."))
 }
