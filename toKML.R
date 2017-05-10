e8ed0ba6f3d6237a4b364667428c6dbf16477a49
diogosbr <diogosbr@gmail.com>
2017-05-10 12:36:00 -0300

    Update toKML.R

--- toKML.R
+++ toKML.R
@@ -1,13 +1,12 @@
-toKML = function(modelo, det.folder = "KML", zeros = FALSE, open = FALSE){
+toKML = function(modelo, name = "meuKML", zeros = FALSE, open = FALSE){
   # instalando pacote
   packages = c("plotKML")
   for (p in setdiff(packages, installed.packages()[, "Package"])) {
     install.packages(p,dependencies = T)
   }
-  if(dir.exists(det.folder)){stop('Esta pasta já existe. Escolha outra.')}else(dir.create(det.folder))
   if(missing(modelo)){stop("Informe o nome do objeto que contem o raster do modelo")}
   if(zeros==FALSE){
     values(modelo)[values(modelo)==0]=NA
-    plotKML(modelo,folder.name=det.folder,open.kml = open)
-  }else(plotKML(modelo,folder.name=det.folder,open.kml = open))
+    plotKML(modelo,folder.name=name,open.kml = open)
+  }else(plotKML(modelo,folder.name=name,open.kml = open))
 }
